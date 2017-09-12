72707069adb0de73678284878c43d1b4a46278e2
Hugo Garcia <hugoallangm@gmail.com>
2017-09-12 12:28:22 -0600

    Ventana con clase

--- ventaHolaMundo.py
+++ ventaHolaMundo.py
@@ -1,8 +1,26 @@
-from Tkinter import *
+import Tkinter as tk
 
-padre = Tk()
+class Aplicacion():
 
-etiqueta = Label(padre, text= " Hola mundo ")
-etiqueta.pack()
+    def __init__(self,master):
+        frame = tk.Frame(master)
+        frame.pack()
 
-padre.mainloop()
+        self.boton = tk.Button(
+            frame, text="Salir", fg ="red", command = frame.quit
+        )
+
+
+        self.boton.pack(side=tk.LEFT)
+        self.botonSaludar = tk.Button(
+            frame, text="Saludar!", fg="blue", command=self.saludar)
+        self.botonSaludar.pack(side=tk.LEFT)
+
+    def saludar(self):
+        self.Etiqueta = tk.Label(text="Hola Mundo Cruel")
+        #self.Etiqueta.pack(side=tk.LEFT)
+        print "Hola Mundo Cruel"
+
+root = tk.Tk()
+app = Aplicacion(root)
+root.mainloop()
